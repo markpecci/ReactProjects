0b9f7a17de555d98955210d2a3fd4559d91cb584
Mark Pecci <mark_pecci@redlands.edu>
2024-12-19 01:21:34 -0800

    new card designs

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,13 @@ function App() {
   const[isVisible, setIsVisible] = useState([false, false, false, false]);
 
   //TODO: make cards in a map and have them more efficiently stored / added
-  /*
+  
   const cardsData = [
     { id: 1, text: 'Card 1', positionClass: 'fade-in-left' },
     { id: 2, text: 'Card 2', positionClass: 'fade-in-right' },
     // You can add more cards here as needed
   ];
-*/
+
   useEffect(() => {
     const handleScroll = () => {
       console.log('Scrolling detected! Current scroll position:', window.scrollY);
@@ -40,19 +40,23 @@ function App() {
       <h1>Mark's Website</h1>
       <div className="card_container">
         <Card         
-        text="Card 1" 
+        title="Card 1" 
+        text="This is what I want in the first card"
         position={isVisible[0] ? "fade-in-left visible-left" : "fade-in-left"} 
         />
         <Card 
-        text="Card 2" 
+        title="Card 2" 
+        text="This is what I want in the second card"
         position={isVisible[1] ? "fade-in-right visible-right" : "fade-in-right"} 
         />
         <Card 
-        text="Card 3" 
+        title="Card 3" 
+        text="This is what I want in the third card"
         position={isVisible[2] ? "fade-in-left visible-left" : "fade-in-left"} 
         />
         <Card 
-        text="Card 4" 
+        title="Card 4" 
+        text="This is what I want in the fourth card"
         position={isVisible[3] ? "fade-in-right visible-right" : "fade-in-right"} 
         />
       </div>
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Card.css'; // Optional: If you want to create a separate style file for cards
 
-function Card({ text, position }) {
+function Card({ title, text, position }) {
 
 
 
   return (
     <div className={`card ${position}`}>
+      <h2>{title}</h2>
       <p>{text}</p>
     </div>
   );
